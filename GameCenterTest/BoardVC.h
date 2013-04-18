d8dc3bddd9ad9b6e3c74a5cc5356936782e0c958
Mikkel Gravgaard <mikkel@klokke.dk>
2013-04-18 14:14:34 +0200

    add reactive cocoa to project

---
+++ GameCenterTest/BoardVC.h
@@ -0,0 +1,16 @@
+//
+// Created by Mikkel Gravgaard on 18/04/13.
+// Copyright (c) 2013 Betafunk. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface BoardVC : UIViewController
+
+
+
+@end
---
+++ GameCenterTest/BoardVC.m
@@ -0,0 +1,19 @@
+//
+// Created by Mikkel Gravgaard on 18/04/13.
+// Copyright (c) 2013 Betafunk. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "BoardVC.h"
+
+
+@implementation BoardVC {
+
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
